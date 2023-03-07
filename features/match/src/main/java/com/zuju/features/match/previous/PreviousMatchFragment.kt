4d228a33dc7b43aaf3088df62ce096f0510244c2
ThanHoang <than_hoang@epam.com>
2023-03-07 14:49:46 +0700

    Add: MediaPlayerActivity

--- features/match/src/main/java/com/zuju/features/match/previous/PreviousMatchFragment.kt
+++ features/match/src/main/java/com/zuju/features/match/previous/PreviousMatchFragment.kt
@@ -1,11 +1,13 @@
 package com.zuju.features.match.previous
 
+import android.content.Intent
 import android.view.LayoutInflater
 import androidx.fragment.app.activityViewModels
 import com.zuju.features.core.base.fragments.BaseFragment
 import com.zuju.features.core.base.utils.observeFlow
 import com.zuju.features.match.databinding.FragmentPreviousBinding
 import com.zuju.features.match.sharedviewmodel.MatchSharedViewModel
+import com.zuju.features.media.video.VideoPlayerActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,9 +22,7 @@ class PreviousMatchFragment : BaseFragment<FragmentPreviousBinding>() {
 
     override fun initView() {
         super.initView()
-        matchAdapter = PreviousMatchAdapter {
-
-        }
+        matchAdapter = PreviousMatchAdapter(::playHighLight)
 
         binding.rcvPreviousMatch.apply {
             adapter = matchAdapter
@@ -36,5 +36,11 @@ class PreviousMatchFragment : BaseFragment<FragmentPreviousBinding>() {
 
     private fun onMatchUpdated(newData: List<PreviousMatchUi>) {
         matchAdapter.submitList(newData)
+    }
+
+    private fun playHighLight(previousMatchUi: PreviousMatchUi) {
+        val intent = Intent(requireContext(), VideoPlayerActivity::class.java)
+        intent.putExtra(VideoPlayerActivity.URL_KEY, previousMatchUi.highlights)
+        requireActivity().startActivity(intent)
     }
 }
---
+++ features/media/src/main/java/com/zuju/features/media/video/VideoPlayerActivity.kt
@@ -0,0 +1,50 @@
+package com.zuju.features.media.video
+
+import android.content.pm.ActivityInfo
+import android.net.Uri
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.zuju.features.media.R
+import com.zuju.features.media.databinding.ActivityVideoPlayerBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class VideoPlayerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityVideoPlayerBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //To play video full screen at the landscape mode
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        binding = ActivityVideoPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        playVideo()
+    }
+
+    private fun playVideo() {
+        val videoUrl = intent.getStringExtra(URL_KEY)
+        videoUrl ?: run {
+            Toast.makeText(this, R.string.not_found_video, Toast.LENGTH_SHORT).show()
+            finish()
+            return
+        }
+
+        val videoView = binding.videoView
+        val uri = Uri.parse(videoUrl)
+        videoView.setVideoURI(uri)
+
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        mediaController.setMediaPlayer(videoView)
+        videoView.setMediaController(mediaController)
+
+        videoView.start()
+    }
+
+    companion object {
+        const val URL_KEY = "video_url"
+    }
+}
