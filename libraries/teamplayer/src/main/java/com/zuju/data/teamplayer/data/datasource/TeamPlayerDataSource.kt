a4ae557076f5ce14c07d282cd5a6112cc4fcc16e
ThanHoang <than_hoang@epam.com>
2023-03-08 01:45:31 +0700

    Refactor: Cache data in TeamPlayerDataSource

--- libraries/teamplayer/src/main/java/com/zuju/data/teamplayer/data/datasource/TeamPlayerDataSource.kt
+++ libraries/teamplayer/src/main/java/com/zuju/data/teamplayer/data/datasource/TeamPlayerDataSource.kt
@@ -4,31 +4,24 @@ import com.zuju.data.teamplayer.data.network.TeamPlayerApiService
 import com.zuju.data.teamplayer.data.network.dto.asDomain
 import com.zuju.data.teamplayer.domain.model.TeamPlayer
 import com.zuju.data.teamplayer.domain.repository.TeamPlayerRepository
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.shareIn
 import javax.inject.Inject
 
 class TeamPlayerDataSource @Inject constructor(
     private val apiService: TeamPlayerApiService,
 ) : TeamPlayerRepository {
 
-    //Assume the data of team player will not be changed frequently. So we gonna cache the loaded data as the sharedFlow
-    @OptIn(DelicateCoroutinesApi::class)
-    private val cachedFlow = flow {
-        val dtoResponse = apiService.getTeams()
-        val result = dtoResponse.teams.map { it.asDomain() }
-        emit(result)
-    }.shareIn(
-        replay = 1,
-        scope = GlobalScope,
-        started = SharingStarted.WhileSubscribed(),
-    )
+    //Assume the data of team player will not be changed frequently. So we gonna cache the loaded data once has the data
+    private lateinit var cachedData: List<TeamPlayer>
 
     override fun getAllTeams(): Flow<List<TeamPlayer>> {
-        return cachedFlow
+        return flow {
+            if (!::cachedData.isInitialized) {
+                val dtoResponse = apiService.getTeams()
+                cachedData = dtoResponse.teams.map { it.asDomain() }
+            }
+            emit(cachedData)
+        }
     }
 }
